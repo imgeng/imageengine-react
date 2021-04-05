78a5f142302161bfc7b5103b38af8b315858bcac
Paul Yermolin <oatto.paul@gmail.com>
2021-04-05 11:46:25 +0200

    feat: incorporate directives

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,10 +1,55 @@
 import { useImageEngineContext } from "../context"
+import { constructUrl } from "../utils"
+
+export type TDirectives = {
+  // Define desired width.
+  width?: number
+  // Set width to auto (with fallback).
+  autoWidthWithFallback?: number
+  // Define desired height.
+  height?: number
+  // Adjust compression.
+  // Possible range: 0-100.
+  compression?: number
+  // Define desired output format.
+  outputFormat?:
+    | "png"
+    | "gif"
+    | "jpg"
+    | "bmp"
+    | "webp"
+    | "jpeg2000"
+    | "svg"
+    | "mp4"
+    | "jxr"
+  // Define desired fit method.
+  fitMethod?: "stretch" | "box" | "letterbox" | "cropbox"
+  // Don't apply any optimizations to the origin image.
+  noOptimization?: true
+  // Adjust sharpness.
+  // Possible range: 0-100.
+  sharpness?: number
+  // Define rotation.
+  // Possible range: -360 to 360.
+  rotate?: number
+  // Use WURFL to calculate screen's width and then scale the image accordingly.
+  // Possible range: 0-100 (float).
+  scaleToScreenWidth?: number
+  // Crop the image [width, height, left, top].
+  crop?: number[]
+  // Convert the image into a data url.
+  inline?: true
+  // Keep EXIF data.
+  keepMeta?: true
+}
 
 type TProps = Omit<JSX.IntrinsicElements["img"], "src"> & {
   src: string
+} & {
+  directives?: TDirectives
 }
 
-const ALLOWED_EXTENSIONS = [
+const ALLOWED_INPUT_EXTENSIONS = [
   "png",
   "gif",
   "jpg",
@@ -16,17 +61,25 @@ const ALLOWED_EXTENSIONS = [
 ]
 
 export function Image(props: TProps): JSX.Element {
-  const { src, ...other } = props
+  const { src, directives, ...other } = props
   const { rootUrl } = useImageEngineContext()
+  const srcWithRootUrl = `${rootUrl}${src}`
 
   const [imageExtension] = src.split(".").slice(-1)
 
-  if (!ALLOWED_EXTENSIONS.includes(imageExtension)) {
+  if (!ALLOWED_INPUT_EXTENSIONS.includes(imageExtension)) {
     console.warn(
       `The following image's extension doesn't match any of the allowed types and won't be optimized: ${src}.`,
-      `List of supported extensions: ${ALLOWED_EXTENSIONS.join(", ")}.`
+      `List of supported extensions: ${ALLOWED_INPUT_EXTENSIONS.join(", ")}.`
     )
   }
 
-  return <img src={`${rootUrl}${src}`} {...other} />
+  return (
+    <img
+      src={
+        directives ? constructUrl(srcWithRootUrl, directives) : srcWithRootUrl
+      }
+      {...other}
+    />
+  )
 }
---
+++ src/utils/index.tsx
@@ -0,0 +1,32 @@
+import { TDirectives } from "../components/Image"
+
+const DIRECTIVE_MAPPING: { [key: string]: string } = {
+  width: "w",
+  autoWidthWithFallback: "w_auto",
+  height: "h",
+  compression: "cmpr",
+  outputFormat: "f",
+  fitMethod: "m",
+  noOptimization: "pass",
+  sharpness: "s",
+  rotate: "r",
+  scaleToScreenWidth: "pc",
+  crop: "cr",
+  inline: "in",
+  keepMeta: "meta",
+}
+
+export function constructUrl(src: string, directives: TDirectives) {
+  const params = Object.entries(directives).reduce((result, [key, value]) => {
+    if (DIRECTIVE_MAPPING[key]) {
+      return result + `/${DIRECTIVE_MAPPING[key]}_${value}`
+    }
+
+    console.warn(
+      `Directive '${key}' isn't recognized and won't be applied to the image.`
+    )
+    return result
+  }, "")
+
+  return `${src}?imgeng=${params}`
+}
