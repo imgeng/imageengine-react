96a01eb176ff80bb524139d1aa2ed42ef318c00f
Paul Yermolin <oatto.paul@gmail.com>
2021-04-05 09:39:24 +0200

    feat: validate image extension

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -4,9 +4,29 @@ type TProps = Omit<JSX.IntrinsicElements["img"], "src"> & {
   src: string
 }
 
+const ALLOWED_EXTENSIONS = [
+  "png",
+  "gif",
+  "jpg",
+  "bmp",
+  "webp",
+  "jpeg2000",
+  "svg",
+  "tif",
+]
+
 export function Image(props: TProps): JSX.Element {
   const { src, ...other } = props
   const { rootUrl } = useImageEngineContext()
+
+  const [imageExtension] = src.split(".").slice(-1)
+
+  if (!ALLOWED_EXTENSIONS.includes(imageExtension)) {
+    console.warn(
+      `The following image's extension doesn't match any of the allowed types and won't be optimized: ${src}.`,
+      `List of supported extensions: ${ALLOWED_EXTENSIONS.join(", ")}.`
+    )
+  }
 
   return <img src={`${rootUrl}${src}`} {...other} />
 }
